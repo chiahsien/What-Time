68b7e7d450ec8f69b1e06ca9cd654b2585dfd081
Nelson Tai <chiahsien@gmail.com>
2021-06-06 13:06:51 +0800

    Add text field bottom line and font size adjustment

--- What Time/ClockTextField.swift
+++ What Time/ClockTextField.swift
@@ -13,9 +13,45 @@ protocol ClockTextFieldDelegate: AnyObject {
 
 final class ClockTextField: UITextField {
     weak var deleteDelegate: ClockTextFieldDelegate?
+    var bottomBorderColor: UIColor? {
+        set {
+            bottomBorder.backgroundColor = newValue
+        }
+        get {
+            return bottomBorder.backgroundColor
+        }
+    }
 
+    private var bottomBorder: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
     override func deleteBackward() {
         deleteDelegate?.textFieldWillDeleteBackward(self)
         super.deleteBackward()
+    }
+}
+
+private extension ClockTextField {
+    func commonInit() {
+        addSubview(bottomBorder)
+        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            bottomBorder.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 1),
+            bottomBorder.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 1),
+            bottomBorder.bottomAnchor.constraint(equalTo: bottomAnchor),
+            bottomBorder.heightAnchor.constraint(equalToConstant: 1)
+        ])
     }
 }
